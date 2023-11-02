e8dd40d114f1686eb84088d752e364a5663e458f
saidureg <saidureg@gmail.com>
2023-11-02 16:25:51 +0600

    fixed some problem

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA3KlqYTAaemX72oHfxILYtrB5aTBIZP9g",
-  authDomain: "gub-eco-system.firebaseapp.com",
-  projectId: "gub-eco-system",
-  storageBucket: "gub-eco-system.appspot.com",
-  messagingSenderId: "890792379884",
-  appId: "1:890792379884:web:68056e0b29765efe1fecc9",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
